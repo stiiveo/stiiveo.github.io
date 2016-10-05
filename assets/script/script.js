140bed7b287643ded99636b38d56c92345122b42
stiiveo <stiiveo@gmail.com>
2016-10-05 20:29:30 +0800

    v0.3
    Add several html pages.

---
+++ assets/script/script.js
@@ -0,0 +1,27 @@
+    // Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon //
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        }
+        else {
+            x.className = "topnav";
+        }
+    }
+    // Automatic Slideshow //
+    var myIndex = 0;
+    carousel();
+
+    function carousel() {
+        var i;
+        var x = document.getElementsByClassName("mySlides");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        myIndex++;
+        if (myIndex > x.length) {
+            myIndex = 1
+        }
+        x[myIndex - 1].style.display = "block";
+        setTimeout(carousel, 5000); // Change image every 2 seconds
+    }
